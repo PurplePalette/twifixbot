8bb324ef70ac05013ca25a0c66bb3645a0daf585
k0tayan <kotayan0415@outlook.jp>
2023-11-12 20:08:51 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# ビルドステージ
+# 使用するGo言語のイメージを指定
+FROM golang:1.21.4 AS builder
+
+# 作業ディレクトリを設定
+WORKDIR /app
+
+# ソースコードの依存関係をコピー
+COPY go.mod go.sum ./
+RUN go mod download
+
+# ソースコードをコピー
+COPY main.go ./
+
+# アプリケーションをビルド
+RUN CGO_ENABLED=0 GOOS=linux go build -o twifixbot
+
+# 実行ステージ
+# 軽量なベースイメージを使用
+FROM alpine:latest
+
+# 作業ディレクトリを設定
+WORKDIR /root/
+
+# ビルドステージから実行可能ファイルをコピー
+COPY --from=builder /app/twifixbot .
+
+# 実行コマンド
+CMD ["/root/twifixbot"]
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Variables used for command line parameters
+var (
+	Token           string
+	twitterURLRegex *regexp.Regexp
+)
+
+func init() {
+
+	Token = os.Getenv("DISCORD_TOKEN")
+	if Token == "" {
+		fmt.Println("No token provided. Please set DISCORD_TOKEN environment variable.")
+		os.Exit(1)
+	}
+
+	twitterURLRegex = regexp.MustCompile(`https:\/\/twitter\.com\/([a-zA-Z0-9_]+)\/status\/(\d+)`)
+}
+
+func main() {
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+
+	if twitterURLRegex.MatchString(m.Content) {
+		// replace twitter.com with vxtwitter.com
+		newUrl := twitterURLRegex.ReplaceAllString(m.Content, "https://vxtwitter.com/$1/status/$2")
+		s.ChannelMessageSend(m.ChannelID, newUrl)
+	}
+}
